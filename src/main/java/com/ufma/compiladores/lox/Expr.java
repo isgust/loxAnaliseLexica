57f235d4d15c44f09bc1f395de2361762ccc1db7
DanielKGM <atbreezy4@gmail.com>
2025-11-10 16:54:51 -0300

    Correção de sintaxe Generate AST e geração do Expr com método accept

--- src/main/java/com/ufma/compiladores/lox/Expr.java
+++ src/main/java/com/ufma/compiladores/lox/Expr.java
@@ -20,6 +20,11 @@ abstract class Expr {
       this.right = right;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
+    }
+
     final Expr left;
     final Token operator;
     final Expr right;
@@ -30,12 +35,22 @@ abstract class Expr {
       this.expression = expression;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
+
     final Expr expression;
   }
 
   static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
     }
 
     final Object value;
@@ -47,7 +62,14 @@ abstract class Expr {
       this.right = right;
     }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+
     final Token operator;
     final Expr right;
   }
+
+  abstract <R> R accept(Visitor<R> visitor);
 }
