4eef5151db35b12008040bc3d40d3f4a36d139ad
isgust <rocha.gustavops@gmail.com>
2025-11-10 16:31:08 -0300

    Add defineVisitor 5.3.3

--- src/main/java/com/ufma/compiladores/tools/GenerateAst.java
+++ src/main/java/com/ufma/compiladores/tools/GenerateAst.java
@@ -28,6 +28,7 @@ public class GenerateAst {
         writer.println("import java.util.List;");
         writer.println();
         writer.println("abstract class " + baseName + " {");
+        defineVisitor(writer, baseName, types);
         // The AST classes.
         for (String type : types) {
             String className = type.split(":")[0].trim();
@@ -38,6 +39,19 @@ public class GenerateAst {
 
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(
+            PrintWriter writer, String baseName, List<String> types) {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("  }");
     }
 
     private static void defineType(
